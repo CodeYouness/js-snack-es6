a6b379c859976f9c4d02aa7c72e6f0e912f64203
CodeYouness <ylijassi@gmail.com>
2024-05-24 18:15:18 +0200

    added bike array

---
+++ snack-3/js/script.js
@@ -0,0 +1,13 @@
+const bike = [
+    { bikeName: "Bianchi", bikeWeight: 7.5 },
+    { bikeName: "Trek", bikeWeight: 8.2 },
+    { bikeName: "Cannondale", bikeWeight: 6.8 },
+    { bikeName: "Specialized", bikeWeight: 7.0 },
+    { bikeName: "Pinarello", bikeWeight: 7.1 },
+    { bikeName: "Scott", bikeWeight: 8.0 },
+    { bikeName: "Giant", bikeWeight: 7.4 },
+    { bikeName: "Merida", bikeWeight: 7.6 },
+    { bikeName: "Canyon", bikeWeight: 7.9 },
+    { bikeName: "BMC", bikeWeight: 8.1 }
+];
+let lightBike;
