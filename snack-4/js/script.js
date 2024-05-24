437e2cd4a0565d0ed1d94e5858e98d0a9d7d733e
CodeYouness <ylijassi@gmail.com>
2024-05-24 18:28:05 +0200

    added football team const

---
+++ snack-4/js/script.js
@@ -0,0 +1,22 @@
+const footballTeam = [
+    { nome: "Atlético Mineiro", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Palmeiras", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Flamengo", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Corinthians", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Fortaleza", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Red Bull Bragantino", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Internacional", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Fluminense", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Cuiabá", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Ceará", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "América Mineiro", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "São Paulo", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Athletico Paranaense", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Atlético Goianiense", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Santos", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Sport Recife", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Bahia", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Grêmio", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Juventude", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Chapecoense", puntiFatti: 0, falliSubiti: 0 }
+];
