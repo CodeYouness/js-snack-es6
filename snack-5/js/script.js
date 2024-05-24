fdc1390d2aed9726c75da3e53432a76319d0407e
CodeYouness <ylijassi@gmail.com>
2024-05-24 19:27:01 +0200

    created new array with  position: random letter

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -4,4 +4,16 @@ function getRandomChar() {
     return letters.charAt(Math.floor(Math.random() * letters.length)).toUpperCase()
 }
 
-console.log(getRandomChar())
+const products = [
+    { name: "Poppy", type: "tshirt", color: "red" },
+    { name: "Jumping", type: "occhiali", color: "blue" },
+    { name: "CrissCross", type: "scarpe", color: "black" },
+    { name: "Jenny", type: "borsa", color: "pink" }
+];
+
+const productWithLabel = products.map((product) => {
+    product.position = getRandomChar()
+    return product
+})
+
+console.log(productWithLabel)
